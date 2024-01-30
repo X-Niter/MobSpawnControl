88016323864d65fd3ec54a24933feee61d03e9f6
xniter <drumsforlife1999@yahoo.com>
2024-01-30 13:23:53 -0600

    isEnabled and spawn rate config options added with updated getters and setters

--- src/main/java/com/github/xniter/nethermobs/ConfigOptions.java
+++ src/main/java/com/github/xniter/nethermobs/ConfigOptions.java
@@ -3,48 +3,45 @@ package com.github.xniter.nethermobs;
 import org.bukkit.entity.EntityType;
 
 public enum ConfigOptions {
-    REPLACE_ORIG_MOB("replace-orig-mobs", false),
-    GHAST("spawn-ghast", true),
-    GHAST_SPAWN_RATE("ghast-spawn-rate", 50),
-    BLAZE("spawn-blaze", true),
-    BLAZE_SPAWN_RATE("blaze-spawn-rate", 50),
-    PIG_ZOMBIE("spawn-pig-zombie", true),
-    PIG_ZOMBIE_SPAWN_RATE("pig-zombie-spawn-rate", 50),
-    MAGMA_CUBE("spawn-magma-cube", true),
-    MAGMA_CUBE_SPAWN_RATE("magma-cube-spawn-rate", 50),
-    SKELETON("spawn-wither-skeleton", true),
-    SKELETON_SPAWN_RATE("wither-skeleton-spawn-rate", 50);
+    REPLACE_ORIG_MOB("replace-orig-mobs", false, 50),
+    GHAST("spawn-ghast", true, 50),
+    GHAST_SPAWN_RATE("ghast-spawn-rate", true, 50),
+    BLAZE("spawn-blaze", true, 50),
+    BLAZE_SPAWN_RATE("blaze-spawn-rate", true, 50),
+    PIG_ZOMBIE("spawn-pig-zombie", true, 50),
+    PIG_ZOMBIE_SPAWN_RATE("pig-zombie-spawn-rate", true, 50),
+    MAGMA_CUBE("spawn-magma-cube", true, 50),
+    MAGMA_CUBE_SPAWN_RATE("magma-cube-spawn-rate", true, 50),
+    SKELETON("spawn-wither-skeleton", true, 50),
+    SKELETON_SPAWN_RATE("wither-skeleton-spawn-rate", true, 50);
 
-    private String name;
-    private Object defaultValue;
-    private Object value;
+    private final String name;
+    private boolean isEnabled;
+    private int spawnRate;
 
-    private ConfigOptions(String name, Object defaultValue) {
+    ConfigOptions(String name, boolean isEnabled, int spawnRate) {
         this.name = name;
-        this.defaultValue = defaultValue;
-    }
-
-    public static boolean isEntityEnabled(EntityType type) {
-        return (Boolean)valueOf(type.name()).getValue();
-    }
-
-    public static int getSpawnRate(EntityType type) {
-        return (Integer)valueOf(String.format("%s_SPAWN_RATE", type.name())).getValue();
+        this.isEnabled = isEnabled;
+        this.spawnRate = spawnRate;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
-    public Object getDefaultValue() {
-        return this.defaultValue;
+    public boolean isEnabled() {
+        return isEnabled;
     }
 
-    public Object getValue() {
-        return this.value;
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
     }
 
-    public void setValue(Object value) {
-        this.value = value;
+    public int getSpawnRate() {
+        return spawnRate;
+    }
+
+    public void setSpawnRate(int spawnRate) {
+        this.spawnRate = spawnRate;
     }
 }
