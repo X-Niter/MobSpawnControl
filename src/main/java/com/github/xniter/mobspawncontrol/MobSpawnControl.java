4b28577bf70f66352581af06d1edcf9013f53663
xniter <drumsforlife1999@yahoo.com>
2024-01-30 22:28:42 -0600

    Fix configs from re creating the config every time the server started.

--- src/main/java/com/github/xniter/mobspawncontrol/MobSpawnControl.java
+++ src/main/java/com/github/xniter/mobspawncontrol/MobSpawnControl.java
@@ -48,35 +48,35 @@ public class MobSpawnControl extends JavaPlugin {
 
             if (!file.exists()) {
                 this.getLogger().info("No config.yml found, creating now");
+                // Add comments
+                this.getConfig().options().header("MobSpawnControl Configuration\n"
+                        + "Set values to true to enable, false to disable\n"
+                        + "Spawn rate is an integer value representing the spawn rate (0 to disable spawning)\n"
+                        + "Allowed worlds is a list of world names where the entity can spawn");
+
+                for (EntityType entityType : EntityType.values()) {
+                    if (entityType.isSpawnable() && entityType.isAlive()) {
+                        List<String> worldNames = new ArrayList<>();
+                        for (World world : Bukkit.getServer().getWorlds()) {
+                            worldNames.add(world.getName());
+                        }
+                        ConfigOptions.addEntityType(entityType, false, 50, worldNames);  // Adjust default values as needed
+                    }
+                }
+
+                for (ConfigOptions option : ConfigOptions.entityConfigMap.values()) {
+                    // Create a section for each entity type
+                    String entitySection = "EntityTypes." + option.getName();
+                    this.getConfig().createSection(entitySection);
+
+                    // Set values for each entity type
+                    this.getConfig().set(entitySection + ".Enabled", option.getDefaultValue());
+                    this.getConfig().set(entitySection + ".SpawnRate", option.getSpawnRate());
+                    this.getConfig().set(entitySection + ".AllowedWorlds", option.getAllowedWorlds());
+                }
             } else {
                 this.getLogger().info("config.yml found!");
-            }
-
-            // Add comments
-            this.getConfig().options().header("MobSpawnControl Configuration\n"
-                    + "Set values to true to enable, false to disable\n"
-                    + "Spawn rate is an integer value representing the spawn rate (0 to disable spawning)\n"
-                    + "Allowed worlds is a list of world names where the entity can spawn");
-
-            for (EntityType entityType : EntityType.values()) {
-                if (entityType.isSpawnable() && entityType.isAlive()) {
-                    List<String> worldNames = new ArrayList<>();
-                    for (World world : Bukkit.getServer().getWorlds()) {
-                        worldNames.add(world.getName());
-                    }
-                    ConfigOptions.addEntityType(entityType, false, 50, worldNames);  // Adjust default values as needed
-                }
-            }
-
-            for (ConfigOptions option : ConfigOptions.entityConfigMap.values()) {
-                // Create a section for each entity type
-                String entitySection = "EntityTypes." + option.getName();
-                this.getConfig().createSection(entitySection);
-
-                // Set values for each entity type
-                this.getConfig().set(entitySection + ".Enabled", option.getDefaultValue());
-                this.getConfig().set(entitySection + ".SpawnRate", option.getSpawnRate());
-                this.getConfig().set(entitySection + ".AllowedWorlds", option.getAllowedWorlds());
+                this.saveConfig();
             }
 
             this.saveConfig();
